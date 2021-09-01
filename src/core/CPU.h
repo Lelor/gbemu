54053232218748afbe2d925611d95992749ea4f8
Lelor <leonardo.rodrigues@sensedata.com.br>
2021-09-01 14:22:57 -0300

    instantiating the CPU on the main script

--- src/core/CPU.h
+++ src/core/CPU.h
@@ -1,6 +1,17 @@
 #pragma once
 #include "RAM.h"
 
+struct Registers
+{
+    unsigned char A;
+    unsigned char B;
+    unsigned char C;
+    unsigned char D;
+    unsigned char E;
+    unsigned char F;
+    unsigned char H;
+    unsigned char L;
+};
 
 class CPU
 {
@@ -9,6 +20,7 @@ public:
     unsigned char tick(unsigned char opCode);
     unsigned char stackPointer;
     unsigned short programCounter;
+    Registers r;
 private:
     RAM& m_ram;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include "core/CPU.h"
+
 
 
 int main()
 {
     std::cout << "hello, world" << std::endl;
+    CPU cpu = CPU(*(new RAM()));
     return 0;
 };
